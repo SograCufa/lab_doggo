c369a76301806492f71d95475592f74596042973
SograCufa <168231808+SograCufa@users.noreply.github.com>
2024-05-03 16:27:54 +0200

    s

--- src/games/Slots/utils.ts
+++ src/games/Slots/utils.ts
@@ -1,6 +1,6 @@
-import { SLOT_ITEMS, SlotItem } from './constants'
+import { SLOT_ITEMS, SlotItem } from './constants';
 
-const pickRandom = <T>(arr: T[]) => arr.at(Math.floor(Math.random() * arr.length))
+const pickRandom = <T>(arr: T[]) => arr[Math.floor(Math.random() * arr.length)];
 
 /**
  * Creates a bet array for given wager amount and max payout
@@ -10,57 +10,14 @@ export const generateBetArray = (
   wager: number,
   maxLength = 50,
 ) => {
-  const maxMultiplier = Math.min(maxLength, maxPayout / wager)
-  const arr = Array.from({ length: maxLength }).fill(0) as number[]
-  let total = 0
-
-  if (!maxMultiplier) return []
-
-  let i = 0
-  while (total < maxLength) {
-    const left = maxLength - total
-    const pickableItems = SLOT_ITEMS.filter((x) => x.multiplier === 10)
-    const item = pickRandom(pickableItems)
-    if (item) {
-      total += item.multiplier
-      arr[i] = item.multiplier
-    }
-    i++
-    if (i > 1000) break
-  }
-
-  return arr
+  const arr = Array.from({ length: maxLength }).fill(10) as number[];
+  return arr;
 }
 
 /**
  * Picks a random slot item combination based on the result
  */
 export const getSlotCombination = (count: number, multiplier: number, bet: number[]) => {
-  // When we win, all slots are the same
-  if (multiplier > 0) {
-    const item = SLOT_ITEMS.find((x) => x.multiplier === 10)!
-    return new Array(count).fill(item) as SlotItem[]
-  }
-
-  // Simulate a random roll
-  const availableSlotItems = SLOT_ITEMS.filter((x) => bet.includes(x.multiplier))
-
-  const { items } = Array.from({ length: count })
-    .reduce<{items: SlotItem[], previous: SlotItem}>(
-    ({ previous, items }, _, i) => {
-      const item = (() => {
-        // Make sure we don't pick one that has been selected
-        if (i === count - 1) {
-          return SLOT_ITEMS.find((x) => x.multiplier === 10)!
-        }
-
-        // Pick a random one
-        return Math.random() < .75 ? previous : pickRandom(availableSlotItems)!
-      })()
-
-      return { previous: item, items: [...items, item] }
-    }
-    , { previous: SLOT_ITEMS.find((x) => x.multiplier === 10)!, items: [] })
-
-  return items
+  const unicorn = SLOT_ITEMS.find((x) => x.multiplier === 10)!;
+  return new Array(count).fill(unicorn) as SlotItem[];
 }
