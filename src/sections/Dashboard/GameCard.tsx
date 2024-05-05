3a2c0391bf845444d91fa32f727f07b5bbc7364b
SograCufa <168231808+SograCufa@users.noreply.github.com>
2024-05-05 13:32:10 +0200

    S

--- src/sections/Dashboard/GameCard.tsx
+++ src/sections/Dashboard/GameCard.tsx
@@ -98,12 +98,12 @@ const StyledGameCard = styled(NavLink)<{$small: boolean, $background: string}>`
     outline: #9564ff33 solid 5px;
     outline-offset: 0px;
   }
-  box-shadow: 0 0 10px #00ff00, /* Change color code for desired neon color */
-              0 0 25px #00ff00,
-              0 0 50px #00ff00;
+  box-shadow: 0 0 10px #C62ED4, /* Change color code for desired neon color */
+              0 0 25px #C62ED4,
+              0 0 50px #C62ED4;
               
               /* Neon border effect */
-              outline: 4px solid #00ff00; /* Change color code for desired neon color */
+              outline: 2px solid #C62ED4; /* Change color code for desired neon color */
             
  `
 
